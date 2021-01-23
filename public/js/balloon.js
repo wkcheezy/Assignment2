60a0e3fe8b7f73592cf5ed3a380a64f8ff3d3d84
William Kuzniarz <wkhappiness@gmail.com>
2021-01-22 22:17:41 -0500

    Added data to schema and removal on max height

--- public/js/balloon.js
+++ public/js/balloon.js
@@ -4,13 +4,17 @@ AFRAME.registerComponent('balloon', {
             type: 'number',
             default: 1
         },
-        speed: {
+        duration: {
+            type: 'number',
+            default: 10000
+        },
+        startHeight: {
             type: 'number',
             default: 1
         },
         maxheight: { 
             type: 'number', 
-            default: 1000
+            default: 10
         }
     },
 
@@ -19,28 +23,22 @@ AFRAME.registerComponent('balloon', {
 
         CONTEXT_AF.el.setAttribute('animation', {
             property: 'position.y',
-            from: 1,
-            to: 1000,
+            from: CONTEXT_AF.data.startHeight,
+            to: CONTEXT_AF.data.maxheight,
             loop: false,
-            dur: 200000,
+            dur: CONTEXT_AF.data.duration,
             enabled: true
         });
-        CONTEXT_AF.el.addEventListener('click', function () {
+        //If a click is detected on the balloon
+        CONTEXT_AF.el.addEventListener('click', () => {
+            //TODO: Add points
             CONTEXT_AF.el.remove();
         });
     },
 
-    update: function () {
-      // Do something when component's data is updated.
-    },
-
-    remove: function () {
-      // Do something the component or its entity is detached.
-      //Check balloon's height, and if it's not at max height, add points to total
-    },
-
-    tick: function (time, timeDelta) {
-      // Do something on every scene tick or frame.
-      //TODO: Add check for height, and if the balloon is high enough, remove it
+    tick: function () {
+        if (this.el.getAttribute('position').y >= this.data.maxheight){
+            this.el.remove();
+        }
     }
 });
