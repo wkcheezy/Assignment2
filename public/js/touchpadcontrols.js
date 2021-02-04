29b3234a74e0ca546a0ae7dfa008e3aa3eb3ab7c
William Kuzniarz <wkhappiness@gmail.com>
2021-02-04 00:42:25 -0500

    Removed console logs

--- public/js/touchpadcontrols.js
+++ public/js/touchpadcontrols.js
@@ -4,32 +4,26 @@ AFRAME.registerComponent('touchpadcontrols', {
         //Daydream, Gear VR, Vive
         el.addEventListener('trackpaddown', function () {
             el.emit('teleStart');
-            console.log('trackteleStart');
         });
 
         el.addEventListener('trackpadup', function () {
             el.emit('teleEnd');
-            console.log('trackteleEnd');
         });
         //Oculus Go, WMR
         el.addEventListener('touchpaddown', function () {
             el.emit('teleStart');
-            console.log('touchteleStart');
         });
 
         el.addEventListener('touchpadup', function () {
             el.emit('teleEnd');
-            console.log('touchteleEnd');
         });
         //Oculus Touch
         el.addEventListener('surfacedown', function () {
             el.emit('teleStart');
-            console.log('surfaceteleStart');
         });
 
         el.addEventListener('surfaceup', function () {
             el.emit('teleEnd');
-            console.log('surfaceteleEnd');
         });
     }
 });
